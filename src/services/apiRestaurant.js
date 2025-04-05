b23f08d3622cfe17a840955cdb17006f0889a216
Bharat <btharwani92@gmail.com>
2025-04-05 18:45:59 -0400

    Version 1

---
+++ src/services/apiRestaurant.js
@@ -0,0 +1,60 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://lwnckglxtclwjzeubyef.supabase.co';
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const API_URL = 'https://react-fast-pizza-api.jonas.io/api';
+
+export async function getMenu() {
+  const { data, error } = await supabase.from('menu').select('*');
+  if (error) {
+    console.log(error);
+    throw new Error('Menu data could not be loaded');
+  }
+
+  return data;
+}
+
+export async function getOrder(id) {
+  const res = await fetch(`${API_URL}/order/${id}`);
+  if (!res.ok) throw Error(`Couldn't find order #${id}`);
+
+  const { data } = await res.json();
+  return data;
+}
+
+export async function createOrder(newOrder) {
+  try {
+    const res = await fetch(`${API_URL}/order`, {
+      method: 'POST',
+      body: JSON.stringify(newOrder),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!res.ok) throw Error();
+    const { data } = await res.json();
+    return data;
+  } catch {
+    throw Error('Failed creating your order');
+  }
+}
+
+export async function updateOrder(id, updateObj) {
+  try {
+    const res = await fetch(`${API_URL}/order/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify(updateObj),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!res.ok) throw Error();
+    // We don't need the data, so we don't return anything
+  } catch (err) {
+    throw Error('Failed updating your order');
+  }
+}
